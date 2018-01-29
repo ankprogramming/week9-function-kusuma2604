f6071b8239ada0540386faa55c7e1f280a399140
Unknown <oppof1szaa168@gmail.com>
2018-01-29 18:01:34 +0700

    ทำเส็จแล้วข้อ 9.1

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -2,7 +2,30 @@
 
 using namespace std;
 
+void swap (int *a, int *b) {
+    int kusuma;
+    kusuma = *a;
+    *a =*b;
+    *b =kusuma;
+    }
+
 int main() {
+    int a,b, cmd;
+    cin >>a;
+    cin >> b;
+    do {
+        cout << "manu\n1. swap()\n2. guit()"<<endl;
+        cin >> cmd;
+        if (cmd ==1) {
+        cout <<a<<b ; swap (&a,&b);
+        }else {break;}
+
+    } while(1);
+     return 0;
+
+
+
+
     /*เขียนโปรแกรมรับค่าจำนวนเต็มบวกจากคีย์บอร์ด 2 จำนวน เก็บไว้ในตัวแปร และสลับค่าข้อมูลระหว่างตัวแปรสองตัวแปรดังกล่าว
     โดยกำหนดให้โปรแกรมสลับค่าข้อมูลจนกว่าผู้ใช้งานต้องการออกจากการทำงานของโปรแกรม
     และแบ่งการทำงานของโปรแกรมเป็นโปรแกรมย่อย
@@ -10,5 +33,4 @@ int main() {
 
 
 
-    return 0;
 }
